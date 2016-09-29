1e3d4285a57650879f47dcabb8db12017f0229a7
Zachary Romer <romerzs14@gmail.com>
2016-09-29 00:20:12 -0700

    Began writing spadesVL.py to handle spades call, but turned into a rewrite of virusland.py.  Havent tested at all yet.  Now written to be run as a command-line program to be easier called with bash scripts and whatnot.  A configuration module will probably get included as an option. spadesVL.py will eventually become virusland.py and replace most of the scattered modules, cleaning up the folder structure significantly.

---
+++ src/spadesVL.py
@@ -0,0 +1,135 @@
+#virusland.py
+#Author: Zachary Romer, romerzs14@gmail.com
+#Date 09/28/2016
+
+import glob
+import subprocess
+import os
+
+class OutCalls:
+
+	def __init__(self, config, runName = None):
+		argv         = sys.argv
+		fpath        = argv[4]
+		self.title   = argv[1]
+		self.runName = argv[1]
+		self.seq1    = argv[2]
+		self.seq2    = argv[3]
+		self.runName = runName
+
+		self.vlhome = os.path.abspath(os.path.join(self.cwd,os.pardir))
+
+		self.indexFolder  = self.vlhome+'/indexes'
+		self.paudaindex   = self.indexFolder+'/paudaindex/'
+		self.lambindex    = self.indexFolder+'/lambdaindex/'
+		self.taxindex     = self.indexFolder+'/taxIndex/' 		#taxFromGBK genomeTax file outputs here
+		self.gbk          = self.indexFolder+'/gbk/'			#folder for gbkList.txt
+		self.faa          = self.indexFolder+'/faa/'			#folder containing allFAAs.faa and faaList.txt
+		self.faalist      = self.faa+'faaList.txt'	#compiled list of faa file locations
+		self.faaAll       = self.faa + 'allFAAs.faa'	#concatenated faa files
+		self.taxIndexFile = self.taxindex+'taxFromGBK.txt'
+		self.gbkList      = self.gbk+'gbkList.txt' 	#compiled list of gbk file locations
+		
+
+		self.runs       = self.vlhome+'/runs'
+		self.runFolder  = self.runs+'/'+self.runName 	#folder for each run
+
+		self.assemblies = self.runFolder+'/assemblies'
+		self.lambda_out = self.runFolder+'/lambda/'
+		self.orfs       = self.runFolder+'/ORFs/'
+		self.pauda_out  = self.runFolder+'/pauda/'
+		self.stats      = self.runFolder+'/stats/'
+		self.kronaFiles = self.runFolder+'/kronaFiles/'
+		self.paudaloc   = None
+
+	def getArgs(self):
+		#need to use getopt module to get command-line options
+		return self
+
+	def callSpades(seq1, seq2, outPath, k=None, meta=False):
+
+		spadesPath = os.getcwd()+'/spades/spades.py'
+
+		if not k:
+			k = '21,33,55,77,99,127'
+
+		if meta:
+			assemblerOption = '--meta'
+		else:
+			assemblerOption = '--only-assembler'
+
+		os.system('{spadesPath} -k {k} {assemblerOption} -1 {seq1} -2 {seq2} -o {outPath}'.format(**locals()))
+
+	def callVelvet(self, fmt, out, seq1, seq2, readtype, kmer):
+
+		read1 = os.path.basename(seq1).split('.')[0]
+		read2 = os.path.basename(seq2).split('.')[0]
+
+		outName   = '{}X{}.fa'.format(read1, read2)
+		velvetOut = '{}/{}X{}'.format(out,read1, read2)
+
+		print "Assembling reads with velvet"
+		print 'Velvet hashing {}'.format(outName)
+		subprocess.call(['velveth', velvetOut, kmer, fmt, readtype, seq1, seq2])
+		
+		print 'Velvet graphing {}'.format(outName)
+		os.system('velvetg {} -exp_cov auto'.format(velvetOut))
+
+		contigFile = velvetOut + '/contigs.fa'
+		moveFiles(contigFile, out, keepOld = False)
+
+	def callEmboss(self, assembly_folder, out_dir):
+
+		files = glob.glob(assembly_folder+'/*')
+
+		print 'Executing Emboss ORF prediction'
+
+		for input_file in files:
+			outname = out_dir + os.path.basename(input_file).split('.')[0] + '_ORF.fa'
+			args    = 'getorf -sequence %s -outseq %s -minsize 117 -find 3' % (input_file, outname)
+            print(args)
+            subprocess.call(args, shell=True)
+
+    def callPauda(self, orf_folder=self.orfs, file_name, pauda_dir, pauda_out=self.pauda_out, pauda_index=self.paudaindex, pauda_loc = self.paudaloc, ):
+    
+    	print 'Executing Pauda read mapper'
+    	
+    	blastX_out = pauda_out + os.path.basename(file_name).split('.')[0] + '_pauda.blastx'
+    	args       = [pauda_loc + '/pauda-run', '--slow', inFile, blastX_out, pauda_index]
+
+    	print args
+		subprocess.call(args)
+
+	def callLambda(self, faa_all=self.faaAll, lambda_out = self.lambda_out, input_orfs=self.orfs, parseOutput = True):
+
+		files = glob.glob(input_orfs+'/*')
+		for input_file in files:
+			file_name = os.path.basename(input_file).split('.')[0]
+			out_path  = lambda_out + file_name + '.m8'
+			args = 'sudo lambda -q {} -d {} -o {}'.format(input_file, faa_all, out_path)
+			print 'Outputing Lambda mapping to {}'.format(out_path)
+			print args
+			subprocess.call(args, shell=True)
+			subprocess.call(['sudo', 'chmod', '666', out_path])
+			if parseOutput:
+				parseLambda(out_path)
+
+	def parseLambda(self, file_in):
+
+		parsed_name = file_in.split('.')[0]+'_parsed.m8'
+		f = open(file_in, 'r')
+		o = open(file_in, 'w')
+		while True:
+			line = f.readline()
+			if line == "":
+				f.close()
+				o.close()
+				break
+			output = re.split(r'\t+', line)
+			o.write(output[0]+"\t"+output[1]+"\t"+output[10]+"\n")
+
+	def moveFiles(self, oldLoc, newLoc, keepOld = True):
+		os.system('mv {} {}'.format(oldLoc, newLoc))
+		if not keepOld:
+			os.system('rm -r {}'.format(os.path.dirname(oldLoc)))
+
